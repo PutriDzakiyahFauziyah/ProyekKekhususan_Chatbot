b6ca6f97f57a7a9a4a7192c6ddef47157cc63673
putridzakiyahfauziyah <putrifauziyah02@gmail.com>
2023-06-05 10:45:45 +0700

    first commit

---
+++ bot.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chatbot PNJ</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">Politeknik Negeri Jakarta</div>
+        <div class="form">
+            <div class="bot-inbox inbox">
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="msg-header">
+                    <p>Selamat datang di contact center Politeknik Negeri Jakarta!</p>
+                </div>
+            </div>
+        </div>
+        <div class="typing-field">
+            <div class="input-data">
+                <input id="data" type="text" placeholder="Ketik pesan" required>
+                <button id="send-btn">Send</button>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $("#send-btn").on("click", function(){
+                $value = $("#data").val();
+                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
+                $(".form").append($msg);
+                $("#data").val('');
+                
+                // start ajax code
+                $.ajax({
+                    url: 'message.php',
+                    type: 'POST',
+                    data: 'isi_pesan=' + $value,
+                    success: function(result){
+                        // Jika sukses ambil data, tampung ke dalam variabel balasan
+                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
+                        
+                        // Masukkan ke dalam form chat
+                        $(".form").append($replay);
+                        
+                        // Buat form otomatis scroll ke bawah jika ada pesan baru
+                        $(".form").scrollTop($(".form")[0].scrollHeight);
+                    }
+                });
+            });
+        });
+    </script>
+    
+</body>
+</html>
---
+++ message_get.php
@@ -0,0 +1,24 @@
+<?php
+
+// Persiapan koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "csv_db 6") or die("Database Error");
+
+// Ambil pesan dari AJAX
+$getMesg = mysqli_real_escape_string($conn, $_GET['q']);
+
+// Cek pertanyaan ke dalam tabel
+$check_data = mysqli_query($conn, "SELECT answers FROM faqs WHERE questions LIKE '%$getMesg%' ");
+
+// Jika pertanyaan/data ditemukan, maka tampilkan jawaban
+if(mysqli_num_rows($check_data) > 0){
+    // Hasil query tampung ke dalam variabel data
+    $fetch_data = mysqli_fetch_assoc($check_data);
+
+    // Tampung jawaban ke dalam variabel untuk dikirim kembali ke ajax
+    $replay = $fetch_data['answers'];
+    echo $replay;
+}else{
+    echo "Maaf, kami tidak mengerti pertanyaan anda";
+}
+
+?>
